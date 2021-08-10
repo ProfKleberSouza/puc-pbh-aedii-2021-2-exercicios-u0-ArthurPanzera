20e81efeb9eeb8053c6f21d4bb7a66dc1228fa4b
Arthur Lara <88518267+ArthurPanzera@users.noreply.github.com>
2021-08-10 19:03:25 +0000

    Exercício 5

--- exe05/main.c
+++ exe05/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main()
+{
+    int n1, n2, n3, n4;
+    float nota;
+
+    scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
+
+    nota = (n1 + n2 + n3 + n4)/4.0;
+
+    printf("\n");
+
+    if(nota >= 6){
+        printf("NOTA = %.2f (APROVADO)", nota);
+    }
+
+    else{
+        printf("NOTA = %.2f (REPROVADO)", nota);
+    }
+
+
+    return 0;
 }
